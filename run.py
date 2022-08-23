00f5ce0bf6619cffeb96496fb61200ffaa88bfe7
rol <romanlopatin@gmail.com>
2022-08-23 10:50:29 +0300

    Task 3 + Первел запуск сервера на wsgiref

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from front import routes, fronts
 from my_wsgi_framework import Application
+from wsgiref.simple_server import make_server
 
 application = Application(routes, fronts)
+
+with make_server('', 8080, application) as httpd:
+    print("Запуск на порту 8080...")
+    httpd.serve_forever()
